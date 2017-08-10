351ea4be7adce0fcd4e85dd6942a0fa6239f46e0
Hong-She Liang <starofrainnight@gmail.com>
2017-08-10 17:14:57 +0800

    Bump version: 0.1.5 → 0.1.6

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ requirements = [
 
 setup(
     name='jinja2-pimport',
-    version='0.1.5',
+    version='0.1.6',
     author='Hong-She Liang',
     author_email='starofrainnight@gmail.com',
     maintainer='Hong-She Liang',
