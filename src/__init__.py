f1a8415051889a3edaec94d5a51898ac56a0ce6a
Hong-She Liang <starofrainnight@gmail.com>
2018-04-06 11:13:39 +0800

    Bump version: 0.1.6 → 0.1.7

--- src/__init__.py
+++ src/__init__.py
@@ -4,6 +4,6 @@ from .pimportextension import PImportExtension
 
 __author__ = 'Hong-She Liang'
 __email__ = 'starofrainnight@gmail.com'
-__version__ = '0.1.6'
+__version__ = '0.1.7'
 
 __all__ = ['PImportExtension']
